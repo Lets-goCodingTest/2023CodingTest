6d7448c2c94c79ad678f8b498c742abd1aa2bbc1
JAYUN <83991079+chojangg@users.noreply.github.com>
2023-03-28 23:41:43 +0900

    자릿수 더하기

---
+++ jayun/AddNumber.java
@@ -0,0 +1,12 @@
+package jayun;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(n > 0){
+            answer += n%10;
+            n/=10;
+        }
+        return answer;
+    }
+}
