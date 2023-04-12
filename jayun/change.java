3fe1a5f30398ed9a56e2ab8ee54c581db7b6ade0
JAYUN <83991079+chojangg@users.noreply.github.com>
2023-04-12 23:49:16 +0900

    거스름돈

---
+++ jayun/change.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int[] money) {
+        int[] answer = new int[100001];
+        answer[0] = 1;
+        for(int i : money) {
+            for(int j = i; j <= n; j++) {
+                answer[j] += answer[j-i];
+            }
+        }
+        return answer[n];
+    }
+}
