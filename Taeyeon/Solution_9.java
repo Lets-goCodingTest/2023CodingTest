7166fed381d8145a0ec450b1e20635207163b3d5
TaeYeon <121775153+xodus5@users.noreply.github.com>
2023-04-12 23:41:02 +0900

    삼총사

---
+++ Taeyeon/Solution_9.java
@@ -0,0 +1,17 @@
+package Taeyeon;
+
+class Solution_9 {
+    public int solution(int[] number) {
+        int answer = 0;
+        for(int i = 0; i < number.length; i++) {
+            for(int j = i + 1; j < number.length; j++) {
+                for(int k = j + 1; k < number.length; k++) {
+                    if(number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
