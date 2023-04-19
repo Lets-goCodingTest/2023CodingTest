257ddc460b13d7366576deb59ccddc5df3e826cf
JAYUN <83991079+chojangg@users.noreply.github.com>
2023-04-19 23:48:53 +0900

    숫자의 표현

---
+++ jayun/numberExpress.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        for(int i = 1; i <= n / 2; i++) {
+            int sum = i;
+            for(int j = i + 1; sum < n; j++) {
+                sum += j;
+            }
+            if(sum == n) answer++;
+        }
+        return answer;
+    }
+}
